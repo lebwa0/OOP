c037f91b6498db8797211eeeeb57f58d2b2ce1e7
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-01 16:07:19 +0300

    Update Task1.java

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -1,1 +1,13 @@
+package task1;
 
+public class Task1 {
+
+    public static void main(String[] args) {
+        // Задаємо розміри прямокутника
+        int length = 15;
+        int width = 20;
+        
+        // Виводимо площу прямокутника
+        System.out.println("Площа кімнати = " + Area.Calculate(length, width) + " m^2");
+    }
+}
