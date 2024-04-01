8f9b6df26558e7fe4f6cd1882ce10208162a66c7
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-01 16:07:33 +0300

    Update Just.java

--- Task1/src/task1/Just.java
+++ Task1/src/task1/Just.java
@@ -1,1 +1,8 @@
+package task1;
 
+public class Just {
+    // Метод для розрахунку площі прямокутника
+    public static float Calculate(int length, int width) {
+        return (float) (length * width);
+    }
+}
