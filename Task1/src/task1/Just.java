f677306d3805e80990c79a6973a877fcbd304b98
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-01 19:11:46 +0300

    ss
    Signed-off-by: Max <060529max@gmail.com>

--- Task1/src/task1/Just.java
+++
@@ -1,8 +0,0 @@
-package task1;
-
-public class AreaCircle {
-    // Метод для розрахунку площі кола
-    public static double calculate(double radius) {
-        return Math.PI * radius * radius;
-    }
-}
--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -1,12 +1,12 @@
 package task1;
 
-public class Task1Circle {
+public class Task1 {
 
     public static void main(String[] args) {
         // Задаємо радіус кола
-        double radius = 5.0;
-        
+        double radius = 4.0;
+
         // Виводимо площу кола
-        System.out.println("Площа кола = " + AreaCircle.calculate(radius) + " m^2");
+        System.out.println("Площа кола = " + Task1Circle.calculateArea(radius) + " m^2");
     }
 }
