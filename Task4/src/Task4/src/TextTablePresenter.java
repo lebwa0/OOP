e90742eb132832ef365bbbc0c280a6bffe96d921
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-03 09:35:03 +0000

    NeYveren

---
+++ Task4/src/Task4/src/TextTablePresenter.java
@@ -0,0 +1,10 @@
+package Task4.src;
+import java.io.Serializable;
+
+
+public class TextTablePresenter implements ResultPresenter {
+    @Override
+    public void presentResults(CalculationDataCollection data, String[] params) {
+        // Ваша реалізація для відображення текстової таблиці
+    }
+}
---
+++ Task4/src/Task4/src/UserInterface.java
@@ -0,0 +1,8 @@
+package Task4.src;
+
+public class UserInterface {
+    public static String[] getMenuSelection() {
+        return null;
+        // Ваша реалізація для взаємодії з користувачем
+    }
+}
