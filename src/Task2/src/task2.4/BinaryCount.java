7577810b28fccbe66417effbe62399b1abfd6dae
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-05 12:44:18 +0000

    4444

--- src/Task2/src/task2.4/BinaryCount.java
+++ src/Task2/src/task2.4/BinaryCount.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 public class BinaryCount {
-    public static int countAlternatingBits(int n) {
+    public static void countAlternatingBits(int n) {
         String binary = Integer.toBinaryString(n);
-        int count = 0;
-        
+        int count0 = 0;
+        int count1 = 0;
+
         // Перевірка чергування 0 та 1 в двійковому представленні
         for (int i = 0; i < binary.length() - 1; i++) {
             if (binary.charAt(i) != binary.charAt(i + 1)) {
-                count++;
+                if (binary.charAt(i) == '0') {
+                    count0++;
+                } else {
+                    count1++;
+                }
             }
         }
-        
-        return count;
+
+        System.out.println("Чергувань числа 0: " + count0);
+        System.out.println("Чергувань числа 1: " + count1);
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть десяткове число: ");
         int decimalNumber = scanner.nextInt(); // Зчитуємо число з клавіатури
-        int alternatingBits = countAlternatingBits(decimalNumber);
-        System.out.println("Кількість чергувань 0 та 1 у двійковому представленні числа " + decimalNumber + " дорівнює: " + alternatingBits);
-        scanner.close(); 
+        countAlternatingBits(decimalNumber);
+        scanner.close(); // Закриваємо Scanner
     }
 }
