70ea11116ded43e02454ffb7ec76ba914c1fcc03
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-05 17:02:10 +0000

    task6

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ package src;
 
 import src.Task6.UserInterface;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.run();
+        ui.run(new Scanner(System.in)); 
     }
 }
--- src/Task6/UserInterface.java
+++ src/Task6/UserInterface.java
@@ -11,22 +11,27 @@ public class UserInterface {
         savedResults = new ArrayList<>();
     }
 
-    public void processUserInput(String input) {
+    public void processUserInput(String input, Scanner scanner) { 
         if (input.equals("1")) {
-            try (Scanner scanner = new Scanner(System.in)) {
-                System.out.print("Введіть десяткове число: ");
-                int decimalNumber = scanner.nextInt();
-                int alternatingBits = BinaryCounter.countAlternatingBits(decimalNumber);
-                System.out.println("Кількість чергувань 0 та 1 у двійковому представленні числа " + decimalNumber + " дорівнює: " + alternatingBits);
-                savedResults.add(decimalNumber);
-                savedResults.add(alternatingBits);
-            }
+            System.out.print("Введіть десяткове число: ");
+            int decimalNumber = scanner.nextInt();
+            scanner.nextLine(); // Споживаємо символ нового рядка
+            int count0 = BinaryCounter.countAlternatingBits(decimalNumber);
+            int count1 = (int) Math.ceil((Math.log(decimalNumber + 1) / Math.log(2))) - count0;
+            System.out.println("Кількість чергувань 0 у двійковому представленні числа " + decimalNumber + " дорівнює: " + count0);
+            System.out.println("Кількість чергувань 1 у двійковому представленні числа " + decimalNumber + " дорівнює: " + count1);
+            savedResults.add(decimalNumber);
+            savedResults.add(count0);
+            savedResults.add(count1);
         } else if (input.equals("2")) {
             System.out.println("Збережені результати:");
-            for (int i = 0; i < savedResults.size(); i += 2) {
+            for (int i = 0; i < savedResults.size(); i += 3) {
                 int number = savedResults.get(i);
-                int count = savedResults.get(i + 1);
-                System.out.println(number + ": " + count);
+                int count0 = savedResults.get(i + 1);
+                int count1 = savedResults.get(i + 2);
+                System.out.println("Число: " + number);
+                System.out.println("Кількість чергувань 0: " + count0);
+                System.out.println("Кількість чергувань 1: " + count1);
             }
         } else if (input.equals("3")) {
             System.out.println("До побачення!");
@@ -35,18 +40,17 @@ public class UserInterface {
             System.out.println("Невірна команда");
         }
     }
+    
 
-    public void run() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Меню:");
-                System.out.println("1. Підрахунок чергувань 0 та 1 у двійковому представленні числа");
-                System.out.println("2. Показати збережені результати");
-                System.out.println("3. Вихід");
-                System.out.print("Оберіть опцію: ");
-                String input = scanner.nextLine();
-                processUserInput(input);
-            }
+    public void run(Scanner scanner) { 
+        while (true) {
+            System.out.println("Меню:");
+            System.out.println("1. Підрахунок чергувань 0 та 1 у двійковому представленні числа");
+            System.out.println("2. Показати збережені результати");
+            System.out.println("3. Вихід");
+            System.out.print("Оберіть опцію: ");
+            String input = scanner.nextLine();
+            processUserInput(input, scanner); 
         }
     }
 }
