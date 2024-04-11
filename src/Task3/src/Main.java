667c8ee3ff544549b773edba0ca20992f9e8ef16
Kuzmenko Max <145370656+lebwa0@users.noreply.github.com>
2024-04-11 09:35:44 +0000

    443453

--- src/Task3/src/Main.java
+++ src/Task3/src/Main.java
@@ -1,5 +1,6 @@
 package src.Task3.src;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -37,6 +38,45 @@ public class Main {
         System.out.println("Кількість чергувань 1: " + alternatingOnes);
         System.out.println("=== Кінець результатів ===");
 
+        // Збереження результатів у файл
+        saveResultsToFile(decimalNumber, binaryString, alternatingZeroes, alternatingOnes);
+
+        // Відновлення результатів з файлу
+        loadResultsFromFile();
+
         scanner.close();
+    }
+
+    private static void saveResultsToFile(int decimalNumber, String binaryString, int alternatingZeroes, int alternatingOnes) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("calculation_result.ser"))) {
+            outputStream.writeInt(decimalNumber);
+            outputStream.writeUTF(binaryString);
+            outputStream.writeInt(alternatingZeroes);
+            outputStream.writeInt(alternatingOnes);
+            System.out.println("Результати обчислень було збережено у файл calculation_result.ser");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadResultsFromFile() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("calculation_result.ser"))) {
+            int decimalNumber = inputStream.readInt();
+            String binaryString = inputStream.readUTF();
+            int alternatingZeroes = inputStream.readInt();
+            int alternatingOnes = inputStream.readInt();
+
+            // Виведення відновлених результатів
+            System.out.println("Тестуємо збереження результатів у файл та їх відновлення:");
+            System.out.println("Результати обчислень було відновлено з файлу calculation_result.ser");
+            System.out.println("= Результати обчислень");
+            System.out.println("Десяткове число: " + decimalNumber);
+            System.out.println("Двійкове представлення: " + binaryString);
+            System.out.println("Кількість чергувань 0: " + alternatingZeroes);
+            System.out.println("Кількість чергувань 1: " + alternatingOnes);
+            System.out.println("=== Кінець результатів ===");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
